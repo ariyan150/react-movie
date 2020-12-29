188f7a2c253937e0b626fd920c767692d4cccdd2
Ariyan Abidi <a.abidi150@gmail.com>
2020-12-29 14:43:25 +0330

    movie search on change

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css"
+
+function Header({user}) {
+    return (
+      <div className="header">
+        <h1>Ariyan</h1>
+      </div>
+    );
+  }
+  
+  export default Header;
+  
---
+++ src/components/Row.js
@@ -0,0 +1,54 @@
+ import { useState, useEffect } from 'react'
+ import "./Row.css"
+
+
+function Row({name}) {
+  const [movies, setmovies] = useState([])
+  const [query, setQuery] = useState([""])
+
+  const onChange = e => {
+    e.preventDefault();
+
+    setQuery(e.target.value);
+
+    fetch(`http://www.omdbapi.com/?s=${e.target.value}&apikey=af3d009a`)
+    .then((res) => res.json())
+    .then((data) => {
+      if(data.Response === 'True') {
+        setmovies(data.Search)
+        console.log(data.Search)
+      } else {
+        setmovies([])
+      }
+      
+    });
+  }
+  
+  
+  
+    return (
+      <div className="containers">
+        <div className='Search bar mt-3'>
+          <input class="form-control"
+          value={query}
+          onChange={onChange} type="text"
+          placeholder="Search for movies"
+          aria-label="default input example">
+          </input>
+        </div>
+
+        <div className="movies" id='row_movies'>
+          {movies.map(movie => (
+            <div className='movie'>
+              <div className='poster'>
+                <img src={movie.Poster} alt={movie.Title} />
+              </div>
+            </div>
+          ))}
+          
+        </div>
+      </div>
+    );
+  }
+  
+  export default Row;
