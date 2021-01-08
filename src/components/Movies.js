ab3f6eb2a335a128a3a247c06867b580bbbdd89c
Ariyan Abidi <a.abidi150@gmail.com>
2021-01-08 23:03:15 +0330

    responsive movie card

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -40,16 +40,9 @@ function Row({setSelectedMovie}) {
         distance: '500px',
         scale: 1
       }
-      const right = {
-        origin: 'right',
-        duration: 2000,
-        delay: 150,
-        distance: '500px',
-        scale: 1
-      }
+      
     sr.reveal('.midle_page', midle);
     sr.reveal('.left_page', left);
-    sr.reveal('.right_page', right);
   },[query])
 
   function truncate(str, n) {
@@ -83,15 +76,17 @@ function Row({setSelectedMovie}) {
                 <div className='title' >
                   {truncate(movie.Title, 10)} ({truncate(movie.Year, 5)})
                 </div>
-                <div className='poster'>
-                  <img src={movie.Poster!='N/A' ? movie.Poster : 'https://images.unsplash.com/photo-1486693326701-1ea88c6e2af3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80' } alt={movie.Title} />
-                </div>
-                <div className='buttons'>
-                  <button type="button" class="btn btn-success" onClick={() => setSelectedMovie(movie.imdbID)}>Info</button>
-                  <button type="button" class="btn btn-light">Add to Watchlist</button>
-                  <button type="button" class="btn btn-danger">Trailers</button>
-                  <button type="button" class="btn btn-primary">Parents Guid</button>
-                  <button type="button" class="btn btn-warning">Movies like this</button>
+                <div className='poster_buttons'>
+                  <div className='poster'>
+                    <img src={movie.Poster!='N/A' ? movie.Poster : 'https://images.unsplash.com/photo-1486693326701-1ea88c6e2af3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80' } alt={movie.Title} />
+                  </div>
+                  <div className='buttons'>
+                    <button type="button" class="btn btn-success" onClick={() => setSelectedMovie(movie.imdbID)}>Info</button>
+                    <button type="button" class="btn btn-light">Watchlist</button>
+                    <button type="button" class="btn btn-danger">Trailers</button>
+                    <button type="button" class="btn btn-primary">Parents</button>
+                    <button type="button" class="btn btn-warning">like this</button>
+                  </div>
                 </div>
               </div>
             ))}
